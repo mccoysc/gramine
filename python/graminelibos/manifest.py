f60803f5b3d16b8278c5ca8b6ed3f40c291aef1b
mccoysc <364589886@qq.com>
2025-11-24 01:47:42 +0800

    Update manifest.py

--- python/graminelibos/manifest.py
+++ python/graminelibos/manifest.py
@@ -350,11 +350,11 @@ class Manifest:
             # set LD_PRELOAD
             manifest['loader']['env']['LD_PRELOAD'] = tmp_preload
             if 'debug' in manifest['sgx']:
-                manifest['loader']['env']['RA_TLS_ALLOW_DEBUG_ENCLAVE_INSECURE'] = '1' if manifest['sgx']['debug']=='true' else '0'
-            manifest['loader']['env']['RATLS_ENABLE_VERIFY'] = '1'
+                manifest['loader']['env']['RA_TLS_ALLOW_DEBUG_ENCLAVE_INSECURE'] = '1'
             manifest['loader']['env']['RA_TLS_ALLOW_OUTDATED_TCB_INSECURE'] = '1'
             manifest['loader']['env']['RA_TLS_ALLOW_HW_CONFIG_NEEDED'] = '1'
             manifest['loader']['env']['RA_TLS_ALLOW_SW_HARDENING_NEEDED'] = '1'
+            manifest['loader']['env']['RATLS_ENABLE_VERIFY'] = '1'
 
             # add mount point for preload in fs.mount
             if not 'fs' in manifest:
